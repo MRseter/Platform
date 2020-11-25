299918597ad413a77e8a9d44f1e658ad39b9e7a9
Fredborg <sophusfs@stud.ntnu>
2020-11-25 14:17:20 +0100

    Tried to clean the Command creation
    Cleaned and restructured the Command creator, specifically how byte arrays are created

--- java/Gyroscope.java
+++ java/Gyroscope.java
@@ -1,35 +1,45 @@
 import java.io.IOException;
 
 public class Gyroscope implements Runnable {
-  private static long readFrec;
+  private static long readTimer;
   private double[] gyroValues;
   private Broadcast broadcaster;
   private SerialCom ser;
   private boolean isReady;
 
-  public Gyroscope(Broadcast broadcaster, long readFreq, String tty) {
+  public Gyroscope(Broadcast broadcaster, long readTimerMillis, String tty) {
     this.broadcaster = broadcaster;
-    readFrec = readFreq;
-    gyroValues = new double[6];
-    isReady = false;
+    this.readTimer = readTimerMillis;
+    this.gyroValues = new double[6];
+    this.isReady = false;
+
     try {
-      ser = new SerialCom(tty, 115200);
+      this.ser = new SerialCom(tty, 115200);
     } catch (IOException IOe) {
       System.out.println(IOe.getMessage());
       throw new RuntimeException("error in gyrothread: during creation" + IOe.getMessage());
     }
   }
 
+  /**
+   * collect and bradcasts data form the serial communication on a set time interval.
+   *
+   */
   public void run() {
     startUp();
+    long start = System.currentTimeMillis();
     while (!Thread.currentThread().isInterrupted()) {
       updateGyroValues();
-      broadcastGyroValues();
-      broadcaster.awaitTimed(readFrec);
+     // broadcastGyroValues();
+
+      broadcaster.awaitTimed(readTimer - (System.currentTimeMillis() - start));
     }
     System.out.println("gyro thread interupted during run time");
   }
 
+  /**
+   * reads the input of the serial gyro comunication until the startup messages are done and prints the startup messages.
+   */
   private void startUp() {
     try {
       String loggerOutput = ser.readLine();
@@ -58,11 +68,17 @@ public class Gyroscope implements Runnable {
       throw new RuntimeException("error in gyrothread when updating values: " + IOe.getMessage());
     }
   }
-
+/*
   private void broadcastGyroValues() {
     broadcaster.send(gyroValues);
   }
+*/
 
+  /**
+   * gets a string array of floating numbers and returns an array of doubles
+   * @param s the string array.
+   * @return a double array with athe values provided.
+   */
   private double[] parseStringArrayToDouble(String[] s) {
     double[] d = new double[s.length];
     for (int i = 0; i < s.length; i++) {
--- java/RoboClaw.java
+++ java/RoboClaw.java
@@ -1,5 +1,3 @@
-import java.nio.ByteBuffer;
-
 /**
  * Handler for commands for a Roboclaw Motor controller;
  * creates commands that can be sent to the controller to control the motors
@@ -41,7 +39,7 @@ public class RoboClaw {
    */
   public byte[] resetEnc1Cmd() {
     cmd = cmds.get(Cmd.SET_ENC1);
-    return getCmdArray(new int[]{address, cmd, 0});
+    return getCmdArray(new byte[]{address, cmd}, 0);
   }
 
   /**
@@ -52,7 +50,7 @@ public class RoboClaw {
    */
   public byte[] resetEnc2Cmd() {
     cmd = cmds.get(Cmd.SET_ENC2);
-    return getCmdArray(new int[]{address, cmd, 0});
+    return getCmdArray(new byte[]{address, cmd});
   }
 
 
@@ -67,7 +65,7 @@ public class RoboClaw {
    */
   public byte[] getEnc1Cmd() {
     cmd = cmds.get(Cmd.READ_ENC1);
-    return getCmdArray(new int[]{address, cmd});
+    return getCmdArray(new byte[]{address, cmd});
   }
 
   /**
@@ -80,7 +78,7 @@ public class RoboClaw {
    */
   public byte[] getEnc2Cmd() {
     cmd = cmds.get(Cmd.READ_ENC2);
-    return getCmdArray(new int[]{address, cmd});
+    return getCmdArray(new byte[]{address, cmd});
   }
 
 
@@ -92,59 +90,55 @@ public class RoboClaw {
    * from one encoder to another. Once a value is sent the motor will begin to accelerate as fast as
    * possible until the defined rate is reached.
    *
-   * @param speed speed of motor
    * @return [Address, 35, Speed(4 Bytes), CRC(2 bytes)]
    */
-   
-  private int addIntToArry(byte[]b, int i,int startPos){
-  byte[] b2 = getBytes(i);
-  for(byte b3:b2){
-  b[startPos] = b3;
-  startPos++;
+
+  private int addIntToArry(byte[] b, int i, int startPos) {
+    byte[] b2 = getIntBytes(i);
+    for (byte b3 : b2) {
+      b[startPos] = b3;
+      startPos++;
+    }
+    return startPos;
   }
-  return startPos;
+
+  private int addShortToArry(byte[] b, int i, int startPos) {
+    byte[] b2 = new byte[]{(byte) ((i >> 8) & 0xff), (byte) ((i) & 0xff)};
+    for (byte b3 : b2) {
+      b[startPos] = b3;
+      startPos++;
+    }
+    return startPos;
   }
-  private int addShortToArry(byte[]b, int i,int startPos){
-  byte[] b2 = new byte[]{(byte)((i>> 8)&0xff),(byte)((i)&0xff)};
-  for(byte b3:b2){
-  b[startPos] = b3;
-  startPos++;
+
+  private int addByteToArry(byte[] b, byte b1, int startPos) {
+    b[startPos] = b1;
+    startPos++;
+    return startPos;
   }
-  return startPos;
-  }
-  
-  private int addByteToArry(byte[]b, byte b1,int startPos){
-  b[startPos] = b1;
-  startPos++;
-  
-  return startPos;
-  }
+
   public byte[] driveM1Cmd(int speed) {
-    byte[]b = new byte[1+1+4+2];
+    int index = 0;
     cmd = cmds.get(Cmd.DRIVE_M1_SIGN_SPD);
-    int pos = 0;
-    pos = addByteToArry(b,address,pos);
-    pos = addByteToArry(b,cmd,pos);
-    pos = addIntToArry(b,speed,pos);
-    
-    int crc = calculateCrc(b,pos);
-    
-    pos = addShortToArry(b,crc,pos);
-    
-    System.out.println(" :: finding cmd " + cmd + "correct size: " + (b.length == (pos)));
+    byte[] portBytes = new byte[]{address, cmd};
+    byte[] b = getCmdArray(portBytes, speed);
+    System.out.println(" :: finding cmd " + cmd + "correct size: " + (b.length == (index)));
     System.out.println(" :: returning cmd");
     printCmd(b);
-    
+
     return b;
   }
-private byte[] getBytes(int i){
-byte[] b =  new byte[]{
-  (byte)((i>>>24)&0xff),
-  (byte)((i>>>16)&0xff),
-  (byte)((i>>>8)&0xff),
-  (byte)((i)&0xff)};
-return b;
-}
+
+  private byte[] getIntBytes(int i) {
+    byte[] b = new byte[]{
+        (byte) ((i >>> 24) & 0xff),
+        (byte) ((i >>> 16) & 0xff),
+        (byte) ((i >>> 8) & 0xff),
+        (byte) ((i) & 0xff)};
+    return b;
+  }
+
+
   /**
    * Drive M2 With Signed Speed
    * Drive M2 using a speed value. The sign indicates which direction the motor will turn. This
@@ -218,104 +212,185 @@ return b;
     return this.stoppBothMotorCommand;
   }
 
-  private byte[] getCmdArray(byte[] int[] cmds) {
-    byte[] ret = addIntsToByteArray(cmds);
-    addCrc(ret);
+  private byte[] getCmdArray(byte[] portBytes, int[] cmdsValues, String order) {
+
+    byte[] ret = buildCmdArray(cmdsValues, portBytes, order);
+    addShortToArry(ret, calculateCrc(ret), portBytes.length + cmdsValues.length - 1);
+    return ret;
+  }
+
+  private byte[] getCmdArray(byte[] portBytes, int cmdValue) {
+
+    byte[] ret = buildCmdArray(cmdValue, portBytes, "114");
+    addShortToArry(ret, calculateCrc(ret), portBytes.length + 4 - 1);
+    return ret;
+  }
+
+  private byte[] getCmdArray(byte[] portBytes) {
+    byte[] ret = new byte[portBytes.length + 2];
+    int i = 0;
+    for (byte b : portBytes) {
+      ret[i] = b;
+      i++;
+    }
+    addShortToArry(ret, calculateCrc(ret), portBytes.length - 1);
+    return ret;
+  }
+
+  private byte[] buildCmdArray(int[] ints, int[] shorts, byte[] bytes, String order) throws IllegalArgumentException {
+    //reserve memory length: 4 bytes per ints, 2 bytes per shorts, 1 byte per bytes, and 2 bytes for crc value at the end.
+    byte[] ret = new byte[ints.length * 4 + shorts.length * 2 + bytes.length + 2];
+    int index = 0;
+    int byteIndex = 0;
+    int intIndex = 0;
+    int shortIndex = 0;
+
+    if (ints.length + shorts.length + bytes.length != order.length() + 2) {
+      throw new IllegalArgumentException("the length of the order of the data does not match the amount of data");
+    }
+    for (char c : order.toCharArray()) {
+      switch (c) {
+        case 1:
+          index = addByteToArry(ret, bytes[byteIndex], index);
+          byteIndex++;
+          break;
+        case 2:
+          index = addShortToArry(ret, shorts[shortIndex], index);
+          shortIndex++;
+          break;
+        case 4:
+          index = addIntToArry(ret, ints[intIndex], index);
+          intIndex++;
+          break;
+        default:
+          throw new IllegalArgumentException("Error: " + c + " is not a valid order argument... " +
+              " the only valid order arguments are 1,2 and 4");
+      }
+
+    }
+    return ret;
+  }
+
+
+  private byte[] buildCmdArray(int[] ints, byte[] bytes, String order) throws IllegalArgumentException {
+
+    //reserve memory length: 4 bytes per ints ,1 byte per bytes, and 2 bytes for crc value at the end
+    byte[] ret = new byte[ints.length * 4 + bytes.length + 2];
+    int index = 0;
+    int byteIndex = 0;
+    int intIndex = 0;
+
+    if (ints.length + bytes.length != order.length() + 2) {
+      throw new IllegalArgumentException("the length of the order of the data does not match the amount of data");
+    }
+    for (char c : order.toCharArray()) {
+      switch (c) {
+        case 1:
+          index = addByteToArry(ret, bytes[byteIndex], index);
+
+          byteIndex++;
+          break;
+        case 4:
+          index = addIntToArry(ret, ints[intIndex], index);
+
+          intIndex++;
+          break;
+        default:
+          throw new IllegalArgumentException("Error: " + c + " is not a valid order argument... " +
+              " the only valid order arguments are 1,2 and 4");
+      }
+
+    }
+    return ret;
+  }
+
+  private byte[] buildCmdArray(int integer, byte[] bytes, String order) throws IllegalArgumentException {
+
+    //reserve memory length: 4 bytes for the int ,1 byte per bytes, and 2 bytes for crc value at the end
+    byte[] ret = new byte[4 + bytes.length];
+    int index = 0;
+    int byteIndex = 0;
+
+    if (1 + bytes.length != order.length()) {
+      throw new IllegalArgumentException("the length of the order of the data does not match the amount of data");
+    }
+    for (char c : order.toCharArray()) {
+      switch (c) {
+        case 1:
+          index = addByteToArry(ret, bytes[byteIndex], index);
+          byteIndex++;
+          break;
+        case 4:
+          index = addIntToArry(ret, integer, index);
+          break;
+        default:
+          throw new IllegalArgumentException("Error: " + c + " is not a valid order argument... " +
+              " the only valid order arguments are 1,2 and 4");
+      }
+
+    }
+    int crc = calculateCrc(ret, index);
+    addShortToArry(ret, crc, index);
     return ret;
   }
 
   private byte[] setDriveMtrCmd(int speed, byte cmd) {
-    byte[] ret =new byte[]{address , cmd};
-    ret = mergeByteArray(ret,addIntsToByteArray(new int[]{speed}));
-    ret = addCrc(ret);
+    byte[] ret = getCmdArray(new byte[]{address, cmd}, new int[]{speed}, "114");
     printCmd(ret);
     System.out.println(" :: returning cmd");
     return ret;
   }
 
-  private void printCmd(byte[] cmd){
-    for(byte b :cmd){
-    System.out.print((b &0xff) + "  ");
-  }
-  
-  System.out.println(" :: ");
-  }
-
-
-  private byte[] addIntsToByteArray(int[] ints) {
-    byte[] byteArray = new byte[]{};
-    for (int i : ints) {
-      mergeByteArray(byteArray, ByteBuffer.allocate(4).putInt(i).array());
-      System.out.println(i);
-      printCmd(ByteBuffer.allocate(4).putInt(i).array());
+  private void printCmd(byte[] cmd) {
+    for (byte b : cmd) {
+      System.out.print((b & 0xff) + "  ");
     }
-    return byteArray;
-
+    System.out.println(" :: ");
   }
+
 
   private byte[] setPosCmd(int encPos, Cmd m) {
+    cmd = cmds.get(m);
     //adding values
-    int[] ints = new int[]{ accel, speed, deccel, encPos};
-    byte[] ret = addIntsToByteArray(ints);
-    ret = mergeByteArray(new byte[]{address,cmd},ret);
-    //buffer
-    addBuffer((byte) 1, ret);
-
-    //crc 16 calculation of command crc16 checksum
-    ret =addCrc(ret);
-
+    byte[] ret = getCmdArray(new byte[]{address, cmd, 1}, new int[]{accel, speed, deccel, encPos}, "1144441");
     return ret;
   }
 
-  private byte[] addCrc(byte[] byteArry) {
-    byte[] bytes = new byte[]{calculateCrc(byteArry)};
-    return mergeByteArray(byteArry, bytes);
-  }
-
-  private void addBuffer(byte buffer, byte[] arrayList) {
-    if (buffer == 0 || buffer == 1) {
-      byte[] bytes = new byte[]{buffer};
-      mergeByteArray(arrayList, bytes);
-    } else throw new IllegalArgumentException("buffer must be 0 for buffering or 1 for overriding last command");
-  }
-
-
   private byte[] stopMotor(byte cmd) {
-    int[] ints = new int[]{address, cmd, 0};
-    return getCmdArray(ints);
-
+    this.cmd = cmd;
+    int[] ints = new int[]{0};
+    return getCmdArray(new byte[]{address, cmd}, ints, "114");
   }
 
-  private byte[] mergeByteArray(byte[] a, byte[] b) {
-    byte[] r = new byte[a.length + b.length];
-    System.arraycopy(a, 0, r, 0, a.length);
-    System.arraycopy(b, 0, r, a.length, b.length);
-    return a;
-  }
 
   private byte[] generateStopAllCommand() {
     cmd = cmds.get(Cmd.DRIVE_FORWARD);
-    int[] ints = new int[]{address, cmd, 0};
-    byte[] ret =getCmdArray(ints);
+    byte[] bytes = new byte[]{address, cmd};
+    byte[] ret = getCmdArray(bytes, 0);
     printCmd(ret);
     return ret;
   }
-  
-    private  byte calculateCrc(byte[] data) {
+
+  private byte calculateCrc(byte[] data) {
     int crc = 0;
     for (int i = 0; i < data.length; i++) {
       crc += (data[i]);
     }
-System.out.println("crc: " + crc);
+    System.out.println("crc: " + crc);
     return (byte) (crc & 0x7f);
-  
-    }
-    private  byte calculateCrc(byte[] data,int nBytes) {
+
+  }
+
+  private byte calculateCrc(byte[] data, int nBytes) {
     int crc = 0;
     for (int i = 0; i < nBytes; i++) {
       crc += (data[i]);
     }
-System.out.println("crc: " + crc);
+    System.out.println("crc: " + crc);
     return (byte) (crc & 0x7f);
+  }
+
+  public byte getLastCmd() {
+    return cmd;
   }
 }
